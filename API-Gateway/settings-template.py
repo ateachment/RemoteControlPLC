dc0637f6da4d85400caf6283570b6005c94d0a62
Wolfhard Eick <w.eick@wvs-ffm.de>
2025-03-27 13:30:28 +0100

    settings-template improved

--- API-Gateway/settings-template.py
+++ API-Gateway/settings-template.py
@@ -1,7 +1,29 @@
 # copy it with your credentials to settings.py
 import os
-PLC_CONFIGS={'SERVER_ADDRESS':'opc.tcp://opcUser:asdUIZFD5367478!!@192.168.178.25:4840/',
-             'SECURITY_STRING':'Basic256Sha256,SignAndEncrypt,PLC-25OPCUA-Client_cert.pem,PLC-25OPCUA-Client_key.pem'}
+PLC_CONFIGS= { 
+    'opcUsers': [
+        'opcUser':'YOUR_OPC_USER',
+        'opcPassword':'YOUR_OPC_USER_PASSWORD'
+        'plcs': [
+            'plc': {
+            'SECURITY_STRING':'Basic256Sha256,SignAndEncrypt,YOUR_CLIENT_CERT_FILE.pem,YOUR_CLIENT_KEY_FILE.pem',
+            'ip:port': 'YOUR_PLC_IP:OPC_PORT'
+            },
+            'plc': {
+            'SECURITY_STRING':'Basic256Sha256,SignAndEncrypt,YOUR_CLIENT_CERT_FILE2.pem,YOUR_CLIENT_KEY_FILE2.pem',
+            'ip:port': 'YOUR_PLC_IP2:OPC_PORT2'
+            }
+        ],
+        'opcUser':'YOUR_OPC_USER2',
+        'opcPassword':'YOUR_OPC_USER_PASSWORD2'
+        'plcs': [
+            'plc': {
+            'SECURITY_STRING':'',
+            'ip:port': 'YOUR_PLC_IP3:OPC_PORT3'
+            }
+        ]
+    ]
+}             
 
 # Set Proxy environment variables to None to bypass system proxy
 os.environ["HTTP_PROXY"] = ""
